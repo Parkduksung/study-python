6274518f2801f54416b469b99d2c7cfab0ad26ec
Parkduksung <duksung1234@naver.com>
2020-12-12 20:17:25 +0900

    new

---
+++ test.py
@@ -0,0 +1,23 @@
+
+
+
+# print("hellow world"[0]) h 만 뽑힘.
+
+# print("hellow world"[-1]) d 뽑힘.
+# print("hello" + 1) 문자열 + int형 은 안된다.
+# print("hello" * 3)  3개 합쳐질때
+
+# print("hello world"[0:5]) hello
+
+# print("hellow world"[0:])  몇번째서부터 나머지 뽑히는거.
+
+# print("hellow world"[:3]) 3번째까지만 뽑히는거.
+
+print(len("hellow world"[0])) 
+
+
+
+
+
+
+
