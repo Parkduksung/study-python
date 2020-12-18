273e6b6d61098bf327147c8ba33fe9765bcb79e8
AuthorName <duksung1234@naver.com>
2020-12-18 23:05:24 +0900

    4-2 장 문제풀이 및 공부

--- chapter4,5.py
+++ chapter4,5.py
@@ -104,3 +104,67 @@ for num in numbers :
 
 print(output)
 
+
+star = "*"
+
+for i in range(5) :
+    print(star)
+    star += "*"
+
+
+# 먼가 mutableMap 이랑 느낌이 비슷함.
+
+
+dictinory = {
+    "name" : "duksung",
+    "height" : 186,
+    "age" : 29,
+    "height" : "188",
+    "like" : ["exercise", "eat food", "coding" , "drinkding"]
+}
+
+print(dictinory["name"])
+print(dictinory["age"])
+print(dictinory["height"])
+print(dictinory["like"][1])
+
+
+
+del dictinory["name"]
+del dictinory["like"]
+
+dictinory["title"] = 1
+
+for key in dictinory : 
+    print(dictinory[key])
+
+#4-2 연습문제
+
+pets = [
+    {"name" : "a" , "age" : 5},
+    {"name" : "b" , "age" : 6},
+    {"name" : "c" , "age" : 7},
+    {"name" : "d" , "age" : 8}
+]
+
+for key in pets:
+    print(key["name"],key["age"])
+
+
+#3번 모르겠음 다시푸셈.
+numbers = [1,2,6,8,4,3,2,1,9,5,4,9,7,2,1,3,5,4,8,9,7,2,3]
+
+
+char = {
+    "name" : "kasa",
+    "level" : 12,
+    "items" : {
+        "sword" : "fire sword",
+        "armor" : "plate"
+    },
+    "skill" : ["t", "k", "o"]
+}
+
+
+for key in char :
+    print(char[key])
