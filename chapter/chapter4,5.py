6c67a9ee1139028ed0f07cf8cd6394563bb8a4dd
Parkduksung <duksung1234@naver.com>
2020-12-20 19:34:54 +0900

    5장 스터디인데 람다가 중요한듯

--- chapter/chapter4,5.py
+++ chapter/chapter4,5.py
@@ -252,6 +252,61 @@ print_("안녕",3)
 
 #파이썬은 switch/case 문이 없다.
 
+from enum import Enum
+class CalType(Enum):
+    Plus = 1
+    Min = 2
+    Mul = 3
+    Div = 4
+
+def operatorCal(kind , a, b) :
+    if kind == CalType.Plus :
+        plus(a,b)
+    elif kind == CalType.Min:
+        min(a,b)
+    elif kind == CalType.Mul:
+        mul(a,b)
+    elif kind == CalType.Div:
+        div(a,b)
+
+def plus(*values) :
+    sum = 0
+    for value in values :
+        sum += value
+    return printResult(sum)
+
+def min(*values) :
+    sum = 0
+    for value in values :
+        if values[0] == value :
+            sum += value
+        else :
+            sum -= value
+    return map(printResult1 , sum)
+
+def mul(*values) :
+    sum = 1
+    for value in values :
+        sum *= value
+    return printResult(sum)
+
+def div(*values) :
+    sum = 0
+    for value in values :
+        if values[0] == value :
+            sum += value
+        else :
+            sum /= value
+    return printResult(sum)
+
+printResult1 = lambda result: print("결과 {}".format(result))
+
+
+def printResult(result) :
+    print("결과 {}".format(result))
+
+operatorCal(CalType.Min,1,2)
+
 #5-1 문제
 
 def mul(*values) :
